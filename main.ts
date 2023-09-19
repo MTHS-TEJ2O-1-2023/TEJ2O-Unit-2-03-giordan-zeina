7dafd265aa84d58afade7489a01026e6977a1944
giordan-zeina <144264315+giordan-zeina@users.noreply.github.com>
2023-09-19 13:27:31 -0400

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Giordan Zeina
+ * Created on: Sep 2023
+ * This program Does Area and Perimeter
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+
+basic.showString('A =' + (5 * 3) .toString())
+
+basic.showString("cm")
