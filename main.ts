1bc26a1910eeaaf886b903a53dd6dd20909ddfc6
giordan-zeina <144264315+giordan-zeina@users.noreply.github.com>
2023-10-15 20:01:47 -0400

    type

--- main.ts
+++ main.ts
@@ -2,10 +2,20 @@
  *
  * Created by: Giordan Zeina
  * Created on: Sep 2023
- * This program Does Area and Perimeter
+ * This program Does Area and Perimeter for a rectangle with the dimensions 5 cm and 3 cm
 */
 
+// Clean up
 basic.clearScreen()
+basic.showIcon(IconNames.Happy)
 basic.pause(1000)
 
-basic.showString("The area of a rectangle with the dimensions 5 cm and 3 cm" + (5 * 3))
+// Area
+basic.clearScreen()
+basic.showString("The area of a rectangle with the dimensions 5 cm and 3 cm is" + (5 * 3) + 'cm^2')
+basic.showIcon(IconNames.Happy)
+
+// Perimeter
+basic.clearScreen()
+basic.showString("The perimeter for a rectangle with the dimensions 5 cm and 3 cm is" + (2 * (5 + 3)) + 'cm')
+basic.showIcon(IconNames.Happy)
