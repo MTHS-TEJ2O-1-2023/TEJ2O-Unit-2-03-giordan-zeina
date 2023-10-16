49006e341075590ff35511f885450530b61874f1
giordan-zeina <144264315+giordan-zeina@users.noreply.github.com>
2023-10-15 20:11:23 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -12,10 +12,10 @@ basic.pause(1000)
 
 // Area
 basic.clearScreen()
-basic.showString("The area of a rectangle with the dimensions 5 cm and 3 cm is" + (5 * 3) + 'cm^2')
+basic.showString('The area of a rectangle with the dimensions 5 cm and 3 cm is' + (5 * 3) + 'cm^2')
 basic.showIcon(IconNames.Happy)
 
 // Perimeter
 basic.clearScreen()
-basic.showString("The perimeter for a rectangle with the dimensions 5 cm and 3 cm is" + (2 * (5 + 3)) + 'cm')
+basic.showString('The perimeter for a rectangle with the dimensions 5 cm and 3 cm is' + (2 * (5 + 3)) + 'cm')
 basic.showIcon(IconNames.Happy)
